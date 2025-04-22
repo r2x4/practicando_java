7b92979182c06a78b42d04c410d77dffea070222
Rodrigo Rocha Sierra <rrs230780@gmail.com>
2025-04-21 20:17:03 -0500

    colombia

---
+++ src/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+    public static void main(String[] args){
+        // Enteros
+        int edad = 44;
+        long numeroGrande = 1000000000L;
+
+        // Decimales
+        double precio = 19.99;
+        float pi = 3.1416f;
+
+        // Texto
+        String nombre = "Rodrigo";
+        char letra = 'R'; // Comillas sencillas para una letra o char
+
+        // Booleano
+        boolean esMayor = true;
+
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + " años");
+
+    }
+}
